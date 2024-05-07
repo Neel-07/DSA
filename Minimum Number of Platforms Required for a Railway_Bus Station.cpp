92b476a0a2513e8da1227f461c0a1497e6f38e6e
Neel Mishra <neelmishra9125@gmail.com>
2024-05-07 08:00:38 +0530

    Create Minimum Number of Platforms Required for a Railway_Bus Station.cpp

---
+++ Minimum Number of Platforms Required for a Railway_Bus Station.cpp
@@ -0,0 +1,48 @@
+// C program to find minimum number of platforms required on 
+// a railway station 
+
+// Importing the required header files 
+#include <stdio.h> 
+
+// Creating MACRO for finding the maximum number 
+#define max(x, y) (((x) > (y)) ? (x) : (y)) 
+
+// Function to find the minimum number of platforms 
+// required 
+int findPlatform(int arr[], int dep[], int n) 
+{ 
+	// plat_needed indicates number of platforms 
+	// needed at a time 
+	int plat_needed = 1, result = 1; 
+
+	// Run a nested for-loop to find the overlap 
+	for (int i = 0; i < n; i++) { 
+
+		// Initially one platform is needed 
+		plat_needed = 1; 
+		for (int j = 0; j < n; j++) { 
+			if (i != j) 
+				// Increment plat_needed when there is an 
+				// overlap 
+				if (arr[i] >= arr[j] && dep[j] >= arr[i]) 
+					plat_needed++; 
+		} 
+
+		// Update the result 
+		result = max(plat_needed, result); 
+	} 
+	return result; 
+} 
+
+// Driver Code 
+int main() 
+{ 
+	// Train 1 => Arrival : 01:00, Departure : 09:00 
+	// Train 2 => Arrival : 03:00, Departure : 04:00 
+	// Train 3 => Arrival : 05:00, Departure : 06:00 
+	int arr[] = { 100, 300, 500 }; 
+	int dep[] = { 900, 400, 600 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	printf("%d", findPlatform(arr, dep, n)); 
+	return 0; 
+}
