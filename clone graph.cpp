073157b00567398dd8ad52e1248a2a5b7c0871b6
Neel Mishra <neelmishra9125@gmail.com>
2024-11-12 23:05:11 +0530

    added clone graph

---
+++ clone graph.cpp
@@ -0,0 +1,34 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() : val(0), neighbors(vector<Node*>()) {}
+    Node(int _val) : val(_val), neighbors(vector<Node*>()) {}
+    Node(int _val, vector<Node*> _neighbors) : val(_val), neighbors(_neighbors) {}
+};
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        unordered_map<Node*, Node*> visited;
+        return dfs(node, visited);
+    }
+
+private:
+    Node* dfs(Node* node, unordered_map<Node*, Node*>& visited) {
+        if (visited.find(node) != visited.end()) {
+            return visited[node];
+        }
+        Node* clone = new Node(node->val);
+        visited[node] = clone;
+        for (Node* neighbor : node->neighbors) {
+            clone->neighbors.push_back(dfs(neighbor, visited));
+        }
+        return clone;
+    }
+};
